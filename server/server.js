d6f36fcff7d143e957e64a6495fa27f6b6d43629
mcbridz@gmail.com <mcbridz@gmail.com>
2021-07-01 13:48:41 -0700

    Typo

--- server/server.js
+++ server/server.js
@@ -5,9 +5,12 @@ const app = express()
 module.exports = function (deps) {
     if (process.env.NODE_ENV === "production") {
         console.log("production mode detected")
+        app.use(express.static(path.join(__dirname, "..", "client", "build")))
         app.get("*", (req, res) => {
             res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"))
         })
+    } else {
+        app.use(express.static("static"))
     }
     const server = require("http").createServer(app)
 
